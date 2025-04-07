9c8f23d8b948c935d910a48fefc07d27da0e7cf3
suraj2012-t <surajt028@gmail.com>
2025-04-07 13:32:55 +0530

    Delete Chrome Extensions/SSO directory

--- Chrome Extensions/SSO/popup.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('clearCookiesButton').addEventListener('click', function() {
-  chrome.runtime.sendMessage({ action: "clearCookiesAndLogout" }, (response) => {
-    if (chrome.runtime.lastError) {
-      console.error('Error:', chrome.runtime.lastError);
-    } else {
-      console.log('Logout response:', response);
-    }
-  });
-});
